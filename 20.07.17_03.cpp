3c76e9bc6bbb233423e11724674c768dfcb0ea0b
김준엽 <68105607+kjystar@users.noreply.github.com>
2020-07-17 22:04:25 +0900

    Add files via upload

---
+++ 20.07.17_03.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int ysalary, msalary;
+	ysalary = 5000;
+	msalary = ysalary / 12;
+	printf("당신의 연봉은 %d\n", ysalary);
+	printf("월 수령액은 %d", msalary);
+	return 0;
+}
